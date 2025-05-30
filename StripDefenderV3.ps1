f91245049e9ec8f8e0b9bb6ee0a147c12c5399d8
tonho911 <148846306+tonho911@users.noreply.github.com>
2025-05-30 08:52:22 +0700

    Update StripDefenderV3.ps1

--- StripDefenderV3.ps1
+++ StripDefenderV3.ps1
@@ -280,6 +280,18 @@ function remove-Defender([String]$folderPath, [String]$edition, [String]$removeD
     Remove-File -path "$removeDir\Windows\System32\SecurityHealth*"
     Remove-File -path "$removeDir\Windows\System32\SecurityCenter*"
     Remove-File -path "$removeDir\Windows\System32\smartscreen.exe" 
+    #Remove-File -path "$removeDir\Windows\System32\CodeIntegrity\CiPolicies\Active\*" 
+
+    #win11 sec app
+    #if ($edition -like '*Windows 11*') {
+    #    Remove-File -path "$removeDir\Program Files\WindowsApps\Microsoft.SecHealthUI_*"
+
+
+    }
+    else {
+
+     #   #win10 sec app
+     #   Remove-File -path "$removeDir\Windows\SystemApps\Microsoft.Windows.SecHealthUI_*"
 
     }
 
@@ -716,4 +728,3 @@ $form.Add_DragDrop({
 
 # Show the form
 $form.ShowDialog() | Out-Null
-
